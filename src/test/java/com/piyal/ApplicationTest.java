a7a0d7af2bbb02ba282ec3353e43635c88543e76
P.S <1998punnam@gmail.com>
2025-02-11 17:57:09 -0800

    Added tests

--- src/test/java/com/piyal/ApplicationTest.java
+++ src/test/java/com/piyal/ApplicationTest.java
@@ -2,10 +2,16 @@ package com.piyal;
 
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.Map;
+
 import jakarta.inject.Inject;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 @MicronautTest
 class ApplicationTest {
@@ -18,4 +24,35 @@ class ApplicationTest {
         Assertions.assertTrue(application.isRunning());
     }
 
+    @Test
+    void testCreateBook(RequestSpecification spec) {
+        System.out.println("\n--- Creating a new book ---");
+        given()
+                .spec(spec)
+                .contentType("application/json")
+                .body(Map.of(
+                        "name", "C++ Guide",
+                        "author", "Doe John",
+                        "price", 229.99,
+                        "totalPage", 400
+                ))
+                .when()
+                .post("/api/v1/book/save")
+                .then()
+                .log().body()  // Print API response
+                .statusCode(200)
+                .body("name", equalTo("C++ Guide"));
+    }
+
+    @Test
+    void testGetAllBooks(RequestSpecification spec) {
+        System.out.println("\n--- Fetching all books ---");
+        given()
+                .spec(spec)
+                .when()
+                .get("/api/v1/book/all")
+                .then()
+                .log().body()  // Print API response
+                .statusCode(200);
+    }
 }
